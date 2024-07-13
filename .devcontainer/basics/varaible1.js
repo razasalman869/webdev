6cc2fe5dd899b89d30a3b28dac63dbbc98d34cba
razasalman869 <155820792+razasalman869@users.noreply.github.com>
2024-07-13 11:05:18 +0000

    variable1

--- .devcontainer/basics/varaible1.js
+++ .devcontainer/basics/varaible1.js
@@ -1,14 +1,53 @@
-const accountId= 1223;
-let accountname= "Salman"
-var accountpass = "12345"
+// const accountId= 1223;
+// let accountname= "Salman"
+// var accountpass = "12345"
 
-accountCity = "sialkot"
-let accountstate;
+// accountCity = "sialkot"
+// let accountstate;
 
-accountId = 323432
-accountname ="hassan"
-accountpass= "4242343"
+// accountId = 323432
+// accountname ="hassan"
+// accountpass= "4242343"
 
-console.log(accountId, accountname, accountpass, accountstate)
+// console.log(accountId, accountname, accountpass,  accountstate)
+// console.table([accountId, accountname, accountpass, ])
+
+// Hitesh Code 
+// const accountId = 144553
+// let accountEmail = "hitesh@google.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+// let accountState;
+
+// // accountId = 2 // not allowed
+
+
+// accountEmail = "hc@hc.com"
+// accountPassword = "21212121"
+// accountCity = "Bengaluru"
+
+// console.log(accountId);
+
+// /*
+// Prefer not to use var
+// because of issue in block scope and functional scope
+// */
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// ######################################################################################
+// Day 1 of 30 days javascript chalenge
+
+// Task 1: 
+var number= 289
+console.log(number)
+let strings = "lorem impsuum loream impsum"
+console.log(strings)
+const boolvar = true
+console.log(boolvar)
+
+
+
 
 
---
+++ .devcontainer/basics/variable1.js
@@ -0,0 +1,14 @@
+const accountId= 1223;
+let accountname= "Salman"
+var accountpass = "12345"
+
+accountCity = "sialkot"
+let accountstate;
+
+accountId = 323432
+accountname ="hassan"
+accountpass= "4242343"
+
+console.log(accountId, accountname, accountpass, accountstate)
+
+
