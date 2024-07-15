51a94997961f898677118a8eba8d62df9612f83b
razasalman869 <155820792+razasalman869@users.noreply.github.com>
2024-07-15 07:19:14 +0000

    day2js

---
+++ .devcontainer/basics/day2.js
@@ -0,0 +1,59 @@
+// Arithmetic Operator Task 1:
+let a = 24;
+let b = 20
+let c= a-b;
+let e= a+b;
+let x = a*b;
+let y= a/b;
+let z= a%b;
+console.log("Sum of two numbers = ", e)
+console.log("Subtraction of two number = ",c)
+console.log("Mutiply two numbers =", x)
+console.log("Division of two numbers =", y)
+console.log("Modulus of two numbers =", z)
+
+// Assignment Operator // Task1:
+let a1 = 10;
+a1 += 2;
+console.log("add number by += operator = ", a1)
+// Task2:
+let a2 = 10;
+a2 -= 2;
+console.log("add number by += operator = ", a2)
+
+// Conparision Operator:  Task1:
+let b1= 34;
+let b2 =23;
+console.log("Comparing two num = ", b1>b2)
+// Task2:
+let b3 = 45;
+let b4 = 45;
+console.log("Comapering two no = ",b3<=b4)
+// Task3:
+const num = 10;
+const str = "10";
+
+console.log(num == str); // true - The values are the same after type conversion
+console.log(num === str); // false - The values are different types and not equal
+// Logical Operator: Task1:
+let c1= 35;
+let c2= 23;
+console.log(c1 > 10 && c1 < 25 )
+console.log(c1 <= 10 || c2 == 23)
+console.log(! (c1 > c2)) 
+
+
+let num1 = 25;
+let result = (num1 > 0) ? "Positive" : (num1 < 0) ? "Negative" : "Zero";
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
