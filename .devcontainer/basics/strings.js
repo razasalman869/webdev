d440e8e12c3b07823e042813c43c83b58bc2b602
razasalman869 <155820792+razasalman869@users.noreply.github.com>
2024-07-30 10:29:01 +0000

    strings

---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,8 @@
+const Firstname = "Salman"
+const salary = 40000
+
+console.log(`My name is ${Firstname} and my salary ${salary}`);
+
+const myname = "salman"
+console.log(myname[0]);
+console.log(myname.length);
