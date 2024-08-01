87b9d5f6a6a91018ceca6d481b9533a0e7fb8a4f
razasalman869 <155820792+razasalman869@users.noreply.github.com>
2024-08-01 10:35:37 +0000

    strings

---
+++ .devcontainer/01_basics/strings.js
@@ -0,0 +1,34 @@
+const Firstname = "Salman"
+const salary = 40000
+
+// console.log(`My name is ${Firstname} and my salary ${salary}`);
+
+const myname = "salman"
+// console.log(myname[0]);
+// console.log(myname.length);
+let gamename = "my name is salman raza "
+// console.log(name.length);
+// console.log(name);
+// console.log(typeof name);
+console.log(gamename.at(0));
+console.log(gamename.slice(-2, 10));
+
+console.log(gamename.toLowerCase());
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
