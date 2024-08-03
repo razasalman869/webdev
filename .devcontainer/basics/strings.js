9b6784f99cc6129a1cb9b0d6ae4b6b01ca0aafc1
Suleman raza <155820792+razasalman869@users.noreply.github.com>
2024-08-03 11:03:28 +0000

    change_in_string

--- .devcontainer/basics/strings.js
+++ .devcontainer/basics/strings.js
@@ -1,8 +1,29 @@
 const Firstname = "Salman"
 const salary = 40000
 
-console.log(`My name is ${Firstname} and my salary ${salary}`);
+// console.log(`My name is ${Firstname} and my salary ${salary}`);
 
-const myname = "salman"
-console.log(myname[0]);
+const myname = "salman raza and i am a developer"
+const desc = "       I am doing web developing using HTMl, CSS < JAVASCRipst    "
+// console.log(myname[0]);
 console.log(myname.length);
+console.log(myname.charAt(0));
+console.log(myname.charCodeAt(2));
+console.log(myname.slice(2,8));
+console.log(myname.slice(-12, -7));
+console.log(myname.substring(0, 10));
+console.log(myname.toUpperCase);
+console.log(myname.toLowerCase);
+console.log(myname.toLocaleLowerCase);
+console.log(desc.trim);
+console.log(desc.trimStart);
+console.log(desc.trimEnd);
+
+
+
+
+
+
+
+
+
