6e451bff556754df32685f97565423ac2e9bec76
Suleman raza <155820792+razasalman869@users.noreply.github.com>
2024-08-08 10:28:07 +0000

    arrays1

---
+++ .devcontainer/basics/arr1.js
@@ -0,0 +1,47 @@
+const myArr = [1,2,3,4,5]
+const arr1 = ['spiderman', 'chach mujri','hero' ]
+const arr2 = new Array(1,2,3,4,)
+
+// console.log(myArr); 
+// console.log(arr1);
+// console.log(arr2);
+// console.log(typeof myArr);
+// console.log(typeof arr1);
+// console.log(typeof arr2);
+
+// Array methods
+
+// myArr.push(2); //add element at end//
+// myArr.pop(); //remove last element from array
+
+// // console.log(myArr);
+// myArr.unshift(4); //add element at start array
+// myArr.shift();    // remove element at start
+// myArr.shift();
+// console.log(myArr);
+
+
+// console.log(myArr.length);
+// console.log(arr1.toString()); //convert array into string comma seprate values
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(1));
+
+
+
+const narr = myArr.join()
+
+console.log(myArr);
+console.log(narr);
+
+
+
+
+
+
+
+
+
+
+
+
